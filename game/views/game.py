93cfc647ca3a8eac80a9585b151c472445f7eb66
Aspect1103 <jack.ashwell1@gmail.com>
2022-02-14 21:07:47 +0000

    Added pretty-format-json pre-commit hook.
    Game now loads tiled maps easily and efficiently.
    
    Next task is physics and player movement.

--- game/views/game.py
+++ game/views/game.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+# Builtin
+from typing import Optional
+
 # Pip
 import arcade
 
@@ -11,24 +14,65 @@ class Game(arcade.View):
     """
     Manages the game and its actions.
 
-    Parameters
+    Attributes
     ----------
-    level: int
-        The level to initialise.
+    tile_map: Optional[arcade.TileMap]
+        The tiled tilemap which represents the current level.
+    wall_list: arcade.SpriteList
+        The sprite list for the floor and crate sprites.
+    coin_list: arcade.SpriteList
+        The sprite list for the coin sprites.
+    enemy_list: arcade.SpriteList
+        The sprite list for the enemies.
+    blocker_list: arcade.SpriteList
+        The sprite list for the walls blocking progression.
+    player_list: arcade.SpriteList
+        The sprite list for the player.
     """
 
-    def __init__(self, level: int) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.tile_map: arcade.TileMap = levels[str(level)]
-        self.scene: arcade.Scene = arcade.Scene.from_tilemap(self.tile_map)
+        self.tile_map: Optional[arcade.TileMap] = None
+        self.wall_list: arcade.SpriteList = arcade.SpriteList(use_spatial_hash=True)
+        self.coin_list: arcade.SpriteList = arcade.SpriteList(use_spatial_hash=True)
+        self.enemy_list: arcade.SpriteList = arcade.SpriteList(use_spatial_hash=True)
+        self.blocker_list: arcade.SpriteList = arcade.SpriteList(use_spatial_hash=True)
+        self.player_list: arcade.SpriteList = arcade.SpriteList(use_spatial_hash=True)
+
+    def setup(self, level: int) -> None:
+        """
+        Sets up the game based on a specific level.
+
+        Parameters
+        ----------
+        level: int
+            The level to load.
+        """
+        # Load the tilemap
+        try:
+            self.tile_map = levels[str(level)]
+        except KeyError:
+            arcade.exit()
+            raise KeyError(f"No map available for level {level}")
+
+        # Load each tilemap layer into its own sprite list
+        self.wall_list = self.tile_map.sprite_lists["Platforms"]
+        self.coin_list = self.tile_map.sprite_lists["Coins"]
+        self.enemy_list = self.tile_map.sprite_lists["Enemies"]
+        self.blocker_list = self.tile_map.sprite_lists["Walls"]
+        self.player_list = self.tile_map.sprite_lists["Player"]
 
         # Set the background color
         arcade.set_background_color(arcade.color.BABY_BLUE)
 
     def on_draw(self) -> None:
-        """ "Render the screen."""
+        """Render the screen."""
         # Clear the screen
         self.clear()
 
-        # Draw the tilemap
-        self.scene.draw()
+        # Draw the sprite lists and player
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.enemy_list.draw()
+        self.blocker_list.draw()
+        self.player_list.draw()
--- game/views/level_selection.py
+++ game/views/level_selection.py
@@ -36,7 +36,8 @@ class LevelButton(arcade.gui.UIFlatButton):
         current_view.manager.disable()
 
         # Create the new game view
-        game_view = Game(int(self.text))
+        game_view = Game()
+        game_view.setup(int(self.text))
         window.views["Game"] = game_view
 
         # Show the game view
