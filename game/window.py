7dc3df24e65112886e520dd7551c7e577b154c11
Aspect1103 <jack.ashwell1@gmail.com>
2022-02-22 11:00:03 +0000

    Added window title.
    Removed a print statement.

--- game/window.py
+++ game/window.py
@@ -14,14 +14,19 @@ class Window(arcade.Window):
     """
     Manages the window and allows switching between views.
 
+    Parameters
+    ----------
+    title: str
+        The title of the window.
+
     Attributes
     ----------
     views: Dict[str, arcade.View]
         Holds all the views used by the game.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, title: str) -> None:
+        super().__init__(title=title)
         self.views: Dict[str, arcade.View] = {}
 
     def __repr__(self) -> str:
@@ -30,7 +35,7 @@ class Window(arcade.Window):
 
 def main() -> None:
     """Initialises the game and runs it."""
-    window = Window()
+    window = Window("Educational Game")
     window.center_window()
     new_view = StartMenu()
     window.views["StartMenu"] = new_view
